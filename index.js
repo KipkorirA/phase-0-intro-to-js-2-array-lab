541a768010150539613071c2cc95d28a1cb3f6cf
Aron <aron.kipkorir@student.moringaschool.com>
2024-06-29 12:50:15 +0300

    Arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+ 
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+
+destructivelyAppendCat= (name)=> cats.push(name);
+  
+destructivelyPrependCat = (name) => cats.unshift(name);
+  
+destructivelyRemoveLastCat = () => cats.pop();
+  
+destructivelyRemoveFirstCat = () => cats.shift();
+  
+appendCat = (name) => {
+    const newCats = [...cats, name];
+    return newCats;
+  }
+  
+prependCat = (name) => {
+    const newCats = [name, ...cats];
+    return newCats;
+  }
+  
+removeLastCat = () => {
+    const newCats = cats.slice(0, -1);
+    return newCats;
+  }
+  
+removeFirstCat = () => {
+    const newCats = cats.slice(1);
+    return newCats;
+  }
+  
